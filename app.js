0c65d50cbb7af423816507d6c87b553f7a823afc
Yann <yann.demaux86@gmail.com>
2023-11-16 10:39:40 +0100

    app listen on port

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
